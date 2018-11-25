619b2247682df6998fa7a8ff4fc3f59d9b3790e5
github username <github email address>
2018-11-24 23:56:12 -0500

    form and posts_new

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
 
 
   renderField = (field) => {
+    const { meta: { touched, error } } = field;
+    const className= `form-group ${touched && error ? 'has-danger' : ''}`;
+
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>
           {field.label}
         </label>
@@ -15,7 +20,9 @@ class PostsNew extends Component {
           type='text'
           {...field.input}
           />
-        {field.meta.error}
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     )
   }
@@ -48,8 +55,8 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
-      <button type='submit' className='btn btn-primary'> Submit
-      </button>
+      <button type='submit' className='btn btn-primary'> Submit</button>
+      <Link to='/' className='btn btn-danger'>Cancel</Link>
       </form>
     );
   }
